2032870daf247118ebc74f6d3b1fe77b4a2ec791
Carrillo-Ch <martin.carrillo@alumnos.uv.cl>
2023-11-24 19:04:42 -0300

    Agregar y elimnar juegos a la lista

--- server/controllers/Lista_juegos.js
+++ server/controllers/Lista_juegos.js
@@ -6,36 +6,41 @@ const db = {
   password: '',
   database: 'progra_web'
 };
-const Agregar= async (req, res) => {
-    const { Nombre_usuario, Nombre_Juego  } = req.params; 
-  
-    try {
-      const connection = await mysql.createConnection(db);
-      const [userData] = await connection.execute('SELECT lv.* FROM lista_videojuegos lv JOIN usuario u ON lv.Id_usuario = u.Id_usuarioWHERE u.Nombre_usuario = ?', [Nombre_usuario]);
-      const Id_lista = userData[0].Id_usuario;
-      const [rows] = await connection.query(' INSERT INTO contiene (Id_lista, Titulo) VALUES ( ?, ?)', [Id_lista, Nombre_Juego]);
-  
-      if (rows.length > 0) {
-        res.status(200).json(rows[0]);
-      } else {
-        res.status(400).json({ error: 'Juego no encontrado' });
-      }
-  
-      await connection.end();
-    } catch (err) {
-      console.error('Error en la consulta:', err);
-      res.status(500).json({ error: 'Error en la consulta a la base de datos' });
+const Agregar = async (req, res) => {
+  const { Nombre_usuario, Nombre_juego } = req.params;
+
+  try {
+    const connection = await mysql.createConnection(db);
+    const [userData] = await connection.execute('SELECT lv.Id_lista FROM lista_videojuegos lv JOIN usuario u ON lv.Id_usuario = u.Id_usuario WHERE u.Nombre_usuario= ?', [Nombre_usuario]);
+    const Id_lista= userData[0].Id_lista;
+
+    const [rows] = await connection.query('INSERT INTO contiene (Id_lista, Titulo) VALUES (?, ?)', [Id_lista, Nombre_juego]);
+    console.log(rows)
+    if (rows.aff > 0) {
+      res.status(200).json(rows[0]);
+    } else {
+      res.status(400).json({ error: 'Juego no encontrado' });
     }
+
+    await connection.end();
+  } catch (err) {
+    console.error('Error en la consulta:', err);
+    res.status(500).json({ error: 'Error en la consulta a la base de datos' });
+  }
 };
 
+
 const Eliminar= async (req, res) => {
-    const { Titulo } = req.params; 
+  const { Nombre_usuario, Nombre_juego } = req.params;
   
     try {
       const connection = await mysql.createConnection(db);
-      const [rows] = await connection.query('SELECT * FROM videojuego WHERE Titulo = ?', [Titulo]);
-  
-      if (rows.length > 0) {
+      const [userData] = await connection.execute('SELECT lv.Id_lista FROM lista_videojuegos lv JOIN usuario u ON lv.Id_usuario = u.Id_usuario WHERE u.Nombre_usuario= ?', [Nombre_usuario]);
+      const Id_lista= userData[0].Id_lista;
+
+      const [rows] = await connection.query('DELETE FROM contiene WHERE Id_lista = ? AND Titulo = ?', [Id_lista, Nombre_juego]);
+
+      if (rows.affectedRows > 0) {
         res.status(200).json(rows[0]);
       } else {
         res.status(400).json({ error: 'Juego no encontrado' });

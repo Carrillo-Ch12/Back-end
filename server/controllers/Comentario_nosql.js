93e86298f6b0f17807b6ac7dd024316356118eea
Carrillo-Ch <martin.carrillo@alumnos.uv.cl>
2023-11-25 01:49:53 -0300

    Comentarios

--- server/controllers/Comentario_nosql.js
+++ server/controllers/Comentario_nosql.js
@@ -1,27 +1,55 @@
-const Comentario = require('../models/Comentario/noqls.js');
+const Comentario = require('../models/Comentario/noqls'); 
 
-const crearComentario = async ( req, res ) => {
+const crearComentario = async (req, res) => {
     try {
+        const { idUsuario, Titulo, Texto } = req.body;
 
-        const data = req.body; //ifnormacion por el cuerpo de http
-        const comentario = new Comentario(data);
-
+        if (!idUsuario || !Titulo || !Texto) {
+            return res.status(400).json({
+                success: false,
+                message: "Los campos idUsuario, Titulo y Texto son obligatorios."
+            });
+        }
+        const comentario = new Comentario({ idUsuario, Titulo, Texto });
         await comentario.save();
-        
+
         return res.status(201).json({
             success: true,
-            message:"Comentario creado"
+            message: "Comentario creado exitosamente",
+            data: comentario
         });
-        
     } catch (error) {
-        console.log(error);
+        console.error(error);
         return res.status(500).json({
             success: false,
             error
         });
-    };
+    }
 };
 
+
+const obtenerComentarios = async (req, res) => {
+    try {
+        const { tituloJuego } = req.params;
+        const comentarios = await Comentario.find({ Titulo: tituloJuego });
+
+        return res.status(200).json({
+            success: true,
+            message: "Comentarios obtenidos",
+            data: comentarios
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            error
+        });
+    }
+};
+
+
+
 module.exports = {
-    crearComentario
-}
+    crearComentario,
+    obtenerComentarios
+};
